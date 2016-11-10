b96533d697ab97e7a321c24e4e403e3a62ce63a8
Rama Varanasi <Rama Varanasi@USRIRILP00045>
2016-11-09 19:19:12 -0600

    Spring

---
+++ SpringWebProjectDemo/src/com/pkg/spring/Address.java
@@ -0,0 +1,29 @@
+package com.pkg.spring;
+
+public class Address {
+
+	String city;
+	String street;
+	String country;
+	
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	
+}
---
+++ SpringWebProjectDemo/src/com/pkg/spring/StudentEditor.java
@@ -0,0 +1,9 @@
+package com.pkg.spring;
+
+public class StudentEditor {
+
+	public StudentEditor() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
